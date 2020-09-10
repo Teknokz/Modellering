7bf266c608611ebc670e3e43b4c217ad65d65b98
Teknokz <60339592+Teknokz@users.noreply.github.com>
2020-09-10 15:55:08 +0200

    test

--- Modelering/Program.cs
+++ Modelering/Program.cs
@@ -12,12 +12,15 @@ namespace Modelering
 
             car c1 = new car();
             car c2 = new car();
+            car c3 = new car();
             
             c1.name = "Toyota Corolla";
             c2.name = "Toyota Prius";
+            c3.name = "Volvo v60";
 
             c1.speed = 42;
             c2.speed = 12;
+            c3.speed = 20;
 
 
             List<car> Toyota = new List<car>();
